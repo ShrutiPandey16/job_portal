4574993dcbda1b016ee8feaef69e2a541381ab8d
shrutiPandey <shrutipandey23456@gmail.com>
2025-07-29 09:55:25 +0530

    Created starter package for job portal

--- section-03-registration/04-jobportal-registration-repositories-and-controllers/src/main/java/com/luv2code/jobportal/repository/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.luv2code.jobportal.repository;
-
-import com.luv2code.jobportal.entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<Users, Integer> {
-}
---
+++ src/main/java/com/learn/jobPortal/controller/HomeController.java
@@ -0,0 +1,5 @@
+package com.learn.jobPortal.controller;
+
+public class HomeController {
+
+}
