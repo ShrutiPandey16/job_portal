e2734394f232453233d6798580b3cb715b9e404a
shrutiPandey <shrutipandey23456@gmail.com>
2025-08-02 19:47:56 +0530

    Added maven dependencies

--- src/main/java/com/learn/jobPortal/controller/HomeController.java
+++ src/main/java/com/learn/jobPortal/controller/HomeController.java
@@ -1,5 +1,13 @@
 package com.learn.jobPortal.controller;
 
-public class HomeController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String Home() {
+		return "index";
+	}
 }
